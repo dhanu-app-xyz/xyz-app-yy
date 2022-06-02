ee340db694b34ceb7007467f7a8a6c30a0c95431
Sarsande-Sunil <93374701+Sarsande-Sunil@users.noreply.github.com>
2022-06-02 14:54:56 +0530

    Apartment-Manager Reupload

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const cors = require("cors");
+
+require("dotenv").config();
+
+// connecting to db
+const connectToDB = require("./configs/db");
+
+connectToDB();
+
+// creating express app
+const app = express();
+
+// port 
+const port = process.env.PORT || 5000;
+
+// global middleware
+app.use(cors());
+app.use(express.json());
+
+
+// importing controllers
+const managerController = require("./controllers/manager.controller");
+const flatController = require("./controllers/flat.controller");
+const residentController = require("./controllers/resident.controller");
+
+
+
+// directing request to respective controllers
+app.use("/manager", managerController);
+app.use("/flats", flatController);
+app.use("/residents", residentController);
+
+
+
+// listening to port 3007
+app.listen(port, () => console.log(`listening on port ${port}`));
