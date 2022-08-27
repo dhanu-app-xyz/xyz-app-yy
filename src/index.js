8da6298c4503527c0975d0ed2231d8b24b937016
Sarsande-Sunil <112194414+dhanu-app-xyz@users.noreply.github.com>
2022-08-27 21:20:35 +0530

    ccc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const express = require("express");
-
 const cors = require("cors");
 
 require("dotenv").config();
-
 // connecting to db
 const connectToDB = require("./configs/db");
-
 connectToDB();
-
 // creating express app
 const app = express();
-
 // port 
 const port = process.env.PORT || 5000;
 
@@ -19,20 +14,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // importing controllers
-const managerController = require("./controllers/manager.controller");
 const flatController = require("./controllers/flat.controller");
-const residentController = require("./controllers/resident.controller");
-
-
 
 // directing request to respective controllers
-app.use("/manager", managerController);
+
 app.use("/flats", flatController);
-app.use("/residents", residentController);
-
-
 
 // listening to port 3007
 app.listen(port, () => console.log(`listening on port ${port}`));
